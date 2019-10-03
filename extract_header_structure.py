d12b3161b43bb1534992b3a22a2a858bf18ac4fc
hoijui <hoijui.quaero@gmail.com>
2019-10-03 08:13:55 +0200

    introduce extract_header_structure

---
+++ extract_header_structure.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+"""
+This is part of the [MoVeDo](https://github.com/movedo) project.
+See LICENSE.md for copyright information.
+
+This extracts the headers and compiles statistical meta-info about them.
+The output is written to a separate file,
+while the AST/the document is not modified at all.
+
+It is implemented as a Pandoc filter using panflute.
+
+This might typicaly be used to gater document strucutre info,
+which would be used by an other filter as input to modfy the document.
+
+Usage example:
+$ pandoc -f markdown -t markdown --atx-headers \
+        -M ehs_output_file="extracted_headers.txt" \
+        --filter extract_header_structure.py \
+        -o output.md \
+        input.md
+"""
+
+# HACK for panflute on python 2
+from __future__ import unicode_literals
+
+import panflute as pf
+from _common import eprint
+
+# constants
+MIN_LEVEL = 1
+# NOTE This will be 10 in future pandoc versions (not yet in pandoc 2.7.3)
+MAX_LEVEL = 6
+MAX_LEVEL = 10
+
+# parameters
+# how many instances of each header level we encountered
+counters = []
+# if True, instead of an exception when resulting header levels are below MIN_LEVEL,
+output_file = ''
+ofh = None
+
+def prepare(doc):
+    """The panflute filter init method."""
+    global counters, output_file, ofh
+    for _ in range(MIN_LEVEL, MAX_LEVEL):
+        counters.append(0)
+    output_file = doc.get_metadata('ehs_output_file', "<ehs_output_file>")
+    ofh = open(output_file, "w")
+
+def action(elem, doc):
+    """The panflute filter main method, called once per element."""
+    global counters
+    if isinstance(elem, pf.Header):
+        lvl = elem.level - MIN_LEVEL
+        counters[lvl] = counters[lvl] + 1
+        ofh.write('%d %s\n' % (lvl + 1, elem.identifier))
+    return elem
+
+def finalize(doc):
+    """The panflute filter "destructor" method."""
+    ofh.write('####\n')
+    total = 0
+    min = 999
+    max = 0
+    for lvl in range(MIN_LEVEL, MAX_LEVEL):
+        cnt = counters[lvl - MIN_LEVEL]
+        total += cnt
+        if lvl > max and cnt > 0:
+            max = lvl
+        if lvl < min and cnt > 0:
+            min = lvl
+    ofh.write('# %s: %d\n' % ("Total headers", total))
+    ofh.write('# %s: %d\n' % ("Min header level", min))
+    ofh.write('# %s: %d\n' % ("Max header level", max))
+    ofh.close()
+
+    pass
+
+def main(doc=None):
+    """
+    NOTE: The main function has to be exactly like this
+    if we want to be able to run filters automatically
+    with '-F panflute'
+    """
+    return pf.run_filter(
+        action,
+        prepare=prepare,
+        finalize=finalize,
+        doc=doc)
+
+if __name__ == '__main__':
+    main()
